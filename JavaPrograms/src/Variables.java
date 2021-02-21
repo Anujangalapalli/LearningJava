e04e0562d3c9d3f068a2ec71fcff1c6b952850fb
Anujangalapalli <anu.jangalapalli@gmail.com>
2021-02-21 14:16:14 -0500

    Printing Variables

---
+++ JavaPrograms/src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables{
+    public static void main(String[] args) {
+        int hour = 11;
+        int minute = 59;
+        System.out.print("The current time is ");
+        System.out.print(hour);
+        System.out.print(":");
+        System.out.print(minute);
+        System.out.println(".");
+    }
+}
