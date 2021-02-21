8496bcff738739f79720cfd4b8858eb3d21578ef
Anujangalapalli <anu.jangalapalli@gmail.com>
2021-02-21 14:19:30 -0500

    Arithmetic Operators

---
+++ JavaPrograms/src/ArithmeticOperators.java
@@ -0,0 +1,8 @@
+public class ArithmeticOperators {
+    public static void main(String[] args){
+        int hour = 11;
+        int minute = 59;
+        System.out.print("No. of minutes since midnight: ");
+        System.out.println(hour * 60 + minute);
+    }
+}
