e9a3ea5fdbed134e8f33cca1e91e1d3389414ff2
Anujangalapalli <anu.jangalapalli@gmail.com>
2021-02-21 12:08:59 -0500

    Hello World2

---
+++ JavaPrograms/src/HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        // generate some simple output
+        System.out.println("Hello, World!"); // first line
+        System.out.println("How are you?"); // another line
+    }
+}
+
