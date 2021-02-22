ae98f2ac47b6758f63d883e9d07e5c929a4919f9
Anujangalapalli <anu.jangalapalli@gmail.com>
2021-02-21 19:41:35 -0500

    Arithmetic Operators

--- JavaPrograms/src/ArithmeticOperators.java
+++ JavaPrograms/src/ArithmeticOperators.java
@@ -1,8 +1,14 @@
 public class ArithmeticOperators {
     public static void main(String[] args){
         int hour = 11;
-        int minute = 59;
+        double minute = 59.0;
         System.out.print("No. of minutes since midnight: ");
         System.out.println(hour * 60 + minute);
+        System.out.print("Fraction of the hour that has passed: ");
+        System.out.println(minute / 60.0);
+        System.out.print("Percent of hour that has passed: ");
+        System.out.println(minute * 100 / 60.0);
+        System.out.println(0.1 * 10);
+        System.out.println(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1);
     }
 }
